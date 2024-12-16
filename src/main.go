3ed56226a290cee1838d9a37aead42046f2af78f
MGavranovic <mgavranovic7@gmail.com>
2024-12-16 11:44:48 +0100

    Basic logging and .gitignore

--- src/main.go
+++ src/main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
+	"log"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello world!")
+	// Creating a log file
+	file, err := os.OpenFile("./logs/server.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("Failed to open the log file: %s", err)
+	}
+	defer file.Close() // Closing the log file
+
+	log.SetOutput(file)         // Setting the outpot to the log file
+	log.Printf("Start logging") // Test printing to the same logfile
 }
